cba991778f60d8331786ae42a0cf897d5db74008
Pankaj <pankajgadge007@gmail.com>
2015-03-11 13:34:18 +0530

    unrelated changes

--- Geolocation/app/src/main/java/pankaj/com/geolocation/MainActivity.java
+++ Geolocation/app/src/main/java/pankaj/com/geolocation/MainActivity.java
@@ -1,53 +1,105 @@
 package pankaj.com.geolocation;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, View.OnClickListener {
 
-    private Button btnShowLoc;
+    private MapFragment mapFragment;
+    private GoogleMap googleMap;
+    private TextView textviewLatitude, textviewLongitude, textviewCountry, textviewCity, textviewPostalCode, textviewAddressLine;
+    private Double latitude, longitude;
+    private Button btnShowLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnShowLoc = (Button) findViewById(R.id.btnShowLocation);
-        btnShowLoc.setOnClickListener(this);
+        btnShowLocation = (Button)findViewById(R.id.btnShowLocation);
+
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        googleMap = mapFragment.getMap();
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        GPSTracker gpsTracker = new GPSTracker(this);
+
+        if (gpsTracker.canGetLocation()) {
+            latitude = gpsTracker.latitude;
+            textviewLatitude = (TextView) findViewById(R.id.fieldLatitude);
+
+            longitude = gpsTracker.longitude;
+            textviewLongitude = (TextView) findViewById(R.id.fieldLongitude);
+
+            String stringLatitude = String.valueOf(gpsTracker.latitude);
+            textviewLatitude.setText(stringLatitude);
+            String stringLongitude = String.valueOf(gpsTracker.longitude);
+            textviewLongitude.setText(stringLongitude);
+
+//            String country = gpsTracker.getCountryName(this);
+//            textviewCountry = (TextView)findViewById(R.id.fieldCountry);
+//            textviewCountry.setText(country);
+//
+//            String city = gpsTracker.getLocality(this);
+//            textviewCity = (TextView)findViewById(R.id.fieldCity);
+//            textviewCity.setText(city);
+//
+//            String postalCode = gpsTracker.getPostalCode(this);
+//            textviewPostalCode = (TextView)findViewById(R.id.fieldPostalCode);
+//            textviewPostalCode.setText(postalCode);
+//
+//            String addressLine = gpsTracker.getAddressLine(this);
+//            textviewAddressLine = (TextView)findViewById(R.id.fieldAddressLine);
+//            textviewAddressLine.setText(addressLine);
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gpsTracker.showSettingsAlert();
+        }
+
+        btnShowLocation.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("Marker"));
+
+    }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, MyLocation.class);
-        startActivity(intent);
-    }
+        GPSTracker gpsTracker1 = new GPSTracker(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        if (gpsTracker1.canGetLocation()) {
+            latitude = gpsTracker1.latitude;
+            longitude = gpsTracker1.longitude;
+            onMapReady(googleMap);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            String stringLatitude = String.valueOf(gpsTracker1.latitude);
+            textviewLatitude.setText(stringLatitude);
+            String stringLongitude = String.valueOf(gpsTracker1.longitude);
+            textviewLongitude.setText(stringLongitude);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gpsTracker1.showSettingsAlert();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- Geolocation/app/src/main/java/pankaj/com/geolocation/MyLocation.java
+++
@@ -1,7 +0,0 @@
-package pankaj.com.geolocation;
-
-/**
- * Created by pankaj on 3/4/2015.
- */
-public class MyLocation {
-}
